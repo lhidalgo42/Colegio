a067e715ec56cc6fbf5e169db440d5f313b51083
Leonardo Hidalgo <leontuna@gmail.com>
2014-11-05 13:38:54 -0200

    add icons to parents

--- app/views/matriculate/partials/parents.blade.php
+++ app/views/matriculate/partials/parents.blade.php
@@ -1,8 +1,8 @@
 <table class="table table-bordered">
   <tr>
-    <td><strong>Familia</strong></td>
+    <td><strong><i class="fa fa-users"></i> Familia</strong></td>
      <td>{{Form::text('familia',Input::old('familia'),array('class' => 'col-md-3 form-control','disabled' => 'disabled')); }}</td>
-    <td><strong>Apoderado Economico</strong></td>
+    <td><strong><i class="fa fa-key"></i> Apoderado Economico</strong></td>
     <td class="text-center">
   <div class="btn-group" data-toggle="buttons">
     <label class="btn btn-primary">
@@ -15,16 +15,16 @@
   </td>
   </tr>
   <tr>
-    <td><strong>Padre</strong></td>
-    <td><a role="button" class="btn btn-primary" >Papa</a></td>
+    <td><strong><i class="fa fa-user"></i> Papa</strong></td>
+    <td class="text-center"><a role="button" class="btn btn-primary" >Agregar</a></td>
     <?php $data_parents = array("rut","nombre","apellido1","apellido2","fecha_nac","profesion","direccion","comuna","email","lugar_de_trabajo","direccion_de_trabajo","telefonos"); ?>
     <?php for($i=0;$i<count($data_parents);$i++){ ?>
     {{Form::hidden('papa_'.$data_parents[$i], Input::old('papa_'.$data_parents[$i])); }}
     <?php } ?>
     {{Form::hidden('papa_sexo','1'); }}
 
-    <td><strong>Madre</strong></td>
-    <td><a  role="button" class="btn btn-primary">Mama</a></td>
+    <td><strong><i class="fa fa-user"></i> Madre</strong></td>
+    <td class="text-center"><a  role="button" class="btn btn-primary"> Agregar</a></td>
         <?php for($i=0;$i<count($data_parents);$i++){ ?>
         {{Form::hidden('mama_'.$data_parents[$i], Input::old('mama_'.$data_parents[$i])); }}
         <?php } ?>
